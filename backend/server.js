5aa14b8ab6855c3e710e109bfc3f9a3365a77877
Jon Perry <jpper@umich.edu>
2025-06-15 14:22:58 -0400

    Set up nodemon along with basic api routes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,19 @@ import express from "express";
 const app = express();
 
 app.get("/api/notes", (req, res) => {
-  res.status(200).send("You got 5 notes");
+  res.status(200).send("You got 20 notes");
+});
+
+app.post("/api/notes", (req, res) => {
+  res.status(201).json({ message: "Post created successfully!" });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Post updated successfully!" });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Note deleted successfully!" });
 });
 
 app.listen(5001, () => {
